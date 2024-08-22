c005e31c6fb8b2c96ffbc4990a983f2cce9935cf
Kooreis <kooreis.official@gmail.com>
2024-08-22 11:13:16 -0500

    Added the enqueue method to the Queue class. This method takes an integer as input and pushes it to stack1. If stack1 is not empty, it pops all elements from stack1 and pushes them to stack2, then pushes the input integer to stack1, and finally pops all elements from stack2 and pushes them back to stack1.

--- solution.java
+++ solution.java
@@ -1,17 +1,11 @@
-import java.util.Stack;
-
-public class Main {
-    public static void main(String[] args) {
-        Queue queue = new Queue();
+void enqueue(int x) {
+    while (!stack1.isEmpty()) {
+        stack2.push(stack1.pop());
     }
-}
 
-class Queue {
-    Stack<Integer> stack1;
-    Stack<Integer> stack2;
+    stack1.push(x);
 
-    Queue() {
-        stack1 = new Stack<>();
-        stack2 = new Stack<>();
+    while (!stack2.isEmpty()) {
+        stack1.push(stack2.pop());
     }
 }
