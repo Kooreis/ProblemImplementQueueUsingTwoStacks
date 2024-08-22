97e1ca2c80d63aab35bbc613ec2d634746c17da9
Kooreis <kooreis.official@gmail.com>
2024-08-22 11:13:16 -0500

    This update adds the Enqueue method to the QueueUsingStacks class. This method takes an integer item as an argument and pushes it onto stack1.

--- solution.cs
+++ solution.cs
@@ -1,14 +1,4 @@
-using System;
-using System.Collections.Generic;
-
-public class QueueUsingStacks
+public void Enqueue(int item)
 {
-    private Stack<int> stack1;
-    private Stack<int> stack2;
-
-    public QueueUsingStacks()
-    {
-        stack1 = new Stack<int>();
-        stack2 = new Stack<int>();
-    }
+    stack1.Push(item);
 }
